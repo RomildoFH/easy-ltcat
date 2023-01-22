886d69ce168fa93fab40df50128c1e9d98ceecbe
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-01-22 18:25:18 -0300

    Cadastrada nova empresa

--- src/data/listaEmpresas.js
+++ src/data/listaEmpresas.js
@@ -114,5 +114,188 @@ export const listaEmpresas = [
       cargo: 'Eng° civil e de segurança do trabalho',
       crea: 'CREA-nº 181682457-7'
     }
+  },
+  {
+    nome: 'GERMANO CABRAL LOPES',
+    nome_fantasia: 'PANIFICADORA LOPES',
+    endereco: 'RUA JOAO GUILHERMINO, 286, CENTRO, ANGELIM - PE',
+    cnpj: '11.546.968/0001-34',
+    versao_do_laudo: '2023.1',
+    ramo: 'Fabricação de produtos de panificação',
+    data_da_pericia: '12/01/2023',
+    horario_inicio: '08:00',
+    horario_termino: '12:00',
+    leis_vigentes: `Leis 8.212 e 8.213/91, Decreto 3048/99 e Instrução Normativa INSS/PRES nº 77 de 21/01/2015, alterada em 26/04/2016`,
+    cnaes: ['10.91-1-02 - Fabricação de produtos de padaria e confeitaria com predominância de produção própria'],
+    grau_de_risco: '3',
+    funcionarios: 2,
+    contato: { nome: 'GERMANO CABRAL LOPES', telefone: '(87) 9.942-7757'},
+    medicoes: ['pressão sonora', 'exposição ao calor', 'agentes químicos', 'biológicos'],
+    escopo_pericia: ['Análise das tarefas executadas', 'Quantificação dos agentes', 'Análise das normas de segurança do trabalho', 'Análise dos sistemas de proteção individual adotados', 'Consulta as normas técnicas'],
+    descricao_do_local: 'A empresa divide-se em dois ambientes, sendo eles a área de vendas de mercadorias e a área de fabricação. Todos os ambientes contam com paredes construídas em alvenaria de tijolis, piso cerâmico na cor branca, telhado em telhas de fibrocimento com pé direito superior a 3 metros de altura.',
+    cargos: [
+      {
+        nome: 'Auxiliar administrativo',
+        atividades: [
+          'Atendimento ao público, controle do fluxo de caixa;',
+          'Auxilia no empacotamento de pães e bolachas;',
+          'Utiliza o fatiador para vendas de congelados ao público;',
+          'Recebe mercadorias e paga fornecedores;',
+        ],
+        agentes_nocivos: [
+          {
+            'Risco Físico': [
+              {
+                'ruido': {
+                  descricao: 'Dentro dos limites de tolerância',
+                  dose: 4.75, // em porcentagem
+                  tempo_amostra: 240, // em minutos
+                  jornada: 8, // em horas
+                  quantidade: '68.0dB(A)',
+                  limite: '85dB(A)',
+                },
+                'calor': {
+                  descricao: 'Dentro dos limites de tolerância',
+                  quantidade: '26,4°',
+                  limite: '28,5°',
+                  carga_solar:false,
+                  ibutg_medio: 29.8,
+                  taxa_metabolica: 258,
+                  jornada: 8,
+                },
+              }
+            ]
+          },
+          {
+            'Risco Químico': [
+              {
+                'Poeiras minerais': {
+                  descricao: 'Não existe',
+                  quantidade: '0 ppm',
+                  limite: 'Não aplicável',
+                }
+              }
+            ]
+          },
+          {
+            'Risco Biológico': [
+              {
+                'vírus': {
+                  descricao: 'Risco Inexistente',
+                  quantidade: 'Não aplicável',
+                  limite: 'Não aplicável',
+                },
+                'bactérias': {
+                  descricao: 'Risco Inexistente',
+                  quantidade: 'Não aplicável',
+                  limite: 'Não aplicável',
+                }
+              }
+            ]
+          }
+        ],
+        medidas_administrativas: ['Não existem medidas administrativas implementadas'],
+        epi: ['Toca higiênica', 'Mascara descartável', 'Luvas latex', 'Calçado de segurança',],
+        epc: ['Não há equipamento de proteção coletiva'],
+        tempo_exposicao: 'Ausência de Agentes Nocivos',
+        exposicao: 'Não se expõem a nenhum agente físico, químico ou biológico, em níveis que venham a causar danos a sua saúde',
+        recomendacoes: ['Realizar atividades de ginástica laboral períodicas', 'Manter equipamentos sempre com a manutenção em dias',],
+      },
+      {
+        nome: 'Padeiro',
+        atividades: [
+          'Separa os ingredientes da mistura, calculando as quantidades e qualidades necessárias para confeccionar a massa;',
+          'Dar tratamentos necessários à massa, fermentando-a, misturando e amassando seus ingredientes, empregando processo manual ou mecânico, a fim de prepará-la para cozimento;',
+          'Cilindrar, cortar e enrolar a massa, procedendo de acordo com a técnica requerida para dar-lhe a forma desejada;',
+          'Cozinhar a massa, levando-a ao forno aquecido a uma temperatura determinada e observando   o   tempo   de   permanência, para   obter   os   pães   na   consistência desejada.',
+        ],
+        agentes_nocivos: [
+          {
+            'Risco Físico': [
+              {
+                'ruido': {
+                  descricao: 'Dentro dos limites de tolerância',
+                  dose: 10, // em porcentagem
+                  tempo_amostra: 240, // em minutos
+                  jornada: 8, // em horas
+                  quantidade: '73.4dB(A)',
+                  limite: '85dB(A)',
+                },
+                'calor': {
+                  descricao: 'Dentro dos limites de tolerância',
+                  quantidade: '26.6°',
+                  limite: '27.9°',
+                  carga_solar:false,
+                  ibutg_medio: 26.6,
+                  taxa_metabolica: 315,
+                  jornada: 8,
+                },
+              }
+            ]
+          },
+          {
+            'Risco Químico': [
+              {
+                'Poeiras minerais': {
+                  descricao: 'Não existe',
+                  quantidade: '0 ppm',
+                  limite: 'Não aplicável',
+                }
+              }
+            ]
+          },
+          {
+            'Risco Biológico': [
+              {
+                'vírus': {
+                  descricao: 'Risco Inexistente',
+                  quantidade: 'Não aplicável',
+                  limite: 'Não aplicável',
+                },
+                'bactérias': {
+                  descricao: 'Risco Inexistente',
+                  quantidade: 'Não aplicável',
+                  limite: 'Não aplicável',
+                }
+              }
+            ]
+          }
+        ],
+        medidas_administrativas: ['Não existem medidas administrativas implementadas'],
+        epi: ['Toca higiênica', 'Mascara descartável', 'Luvas latex', 'Calçado de segurança',],
+        epc: ['Não há equipamento de proteção coletiva'],
+        tempo_exposicao: 'Ausência de Agentes Nocivos',
+        exposicao: 'Não se expõem a nenhum agente físico, químico ou biológico, em níveis que venham a causar danos a sua saúde',
+        recomendacoes: ['Realizar atividades de ginástica laboral períodicas', 'Manter equipamentos sempre com a manutenção em dias',],
+      }
+    ],
+    equipamentos: [
+      {
+        nome: 'Dosimetro',
+        marca: 'Instrutherm',
+        modelo: 'DOS-600',
+        metodo: 'As medições foram realizadas com o instrumento colocado na altura do ouvido humano, estando o mesmo, com circuito de resposta linear.'
+      },
+      {
+        nome: 'Medidor de stress térmico',
+        marca: 'Instrutherm',
+        modelo: 'TGD-400',
+        metodo: 'A medição foi realizada com o instrumento colocado na altura do peito do funcionário, na região de maior incidência de calor.'
+      },
+    ],
+    metodologia: [''],
+    conclusao: 'Seguindo os parâmetros legais, podemos concluir que as atividades de nenhum colaborador provoca exposição a agente nocivo em níveis que venham a causar danos a sua saúde. Portanto, nenhuma atividade se enquandra no Regulamento da Previdência Social para fins de emissão do formulário de informações sobre Atividdades com Expisções a Agentes Agressivos - Aposentadoria Especial do INSS - Instituto Nacional de Seguridade Social.',
+    cidade: 'Algelim - PE',
+    data_conclusao: '12 de Janeiro de 2022',
+    responsavel_empresa: {
+      nome: 'Germano Cabral Lopes',
+      cargo: 'Proprietário',
+      cpf: '100.100.100-00'
+    },
+    responsavel_tecnico: {
+      nome: 'Romildo Pereira da Silva Filho',
+      cargo: 'Eng° civil e de segurança do trabalho',
+      crea: 'CREA-nº 181682457-7'
+    }
   }
 ];
